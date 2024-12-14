2066f553ffb8bc9196bcc6a9e02bb7b358aae91a
Hassan <hassanshakil4569@gmail.com>
2024-12-14 18:11:40 +0500

    chores: enable debugging tools

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,7 @@ const createWindow = (): void => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
   mainWindow.setMenu(null);
+  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
